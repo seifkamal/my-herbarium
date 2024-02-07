d2776c42e1bb474906168d241b03eb2d33eb990c
Seif Kamal <seifskm@gmail.com>
2024-02-07 20:38:55 +0000

    Build Flowers Accordion

--- src/components/ui/Flowers.tsx
+++ src/components/ui/Flowers.tsx
@@ -1,4 +1,10 @@
-import { FC } from "react";
+"use client";
+
+import { useState, type FC } from "react";
+import * as Accordion from "@radix-ui/react-accordion";
+import cn from "classnames";
+import { FlowerCard } from "./FlowerCard";
+import { FlowerMenu } from "./FlowerMenu";
 
 export interface IFlower {
   id: string;
@@ -14,9 +20,54 @@ interface FlowersProps {
 }
 
 export const Flowers: FC<FlowersProps> = ({ flowers }) => (
-  <ol>
+  <Accordion.Root type="single" className="flex flex-col gap-4" collapsible>
     {flowers.map((flower) => (
-      <li key={flower.id}>{flower.name}</li>
+      <Flower key={flower.id} flower={flower} />
     ))}
-  </ol>
+  </Accordion.Root>
 );
+
+interface FlowerProps {
+  flower: IFlower;
+  simple?: boolean;
+}
+
+const Flower: FC<FlowerProps> = ({ flower, simple = false }) => {
+  const { id, name, description, color } = flower;
+  const [temperature, setTemperature] = useState(flower.temperature);
+  const [humidity, setHumidity] = useState(flower.humidity);
+  return (
+    <Accordion.AccordionItem
+      value={id}
+      className={cn(
+        "rounded-2xl",
+        color === "pink" &&
+          "text-pink-dark bg-pink-light dark:text-pink-light dark:bg-pink-dark",
+        color === "blue" &&
+          "text-blue-dark bg-blue-light dark:text-blue-light dark:bg-blue-dark",
+        color === "green" &&
+          "text-green-dark bg-green-light dark:text-green-light dark:bg-green-dark"
+      )}
+    >
+      <Accordion.AccordionTrigger className="w-full">
+        <FlowerCard
+          name={name}
+          color={color}
+          temperature={temperature}
+          humidity={humidity}
+        />
+      </Accordion.AccordionTrigger>
+      <Accordion.Content className="overflow-hidden data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp">
+        <FlowerMenu
+          color={color}
+          simple={simple}
+          description={description}
+          defaultTemperature={temperature}
+          onTemperatureChange={setTemperature}
+          defaultHumidity={humidity}
+          onHumidityChange={setHumidity}
+        />
+      </Accordion.Content>
+    </Accordion.AccordionItem>
+  );
+};
