ea8a36f27369b7b6cf51f57664f16894b8f70d86
Seif Kamal <seifskm@gmail.com>
2024-02-08 10:31:18 +0000

    feat: Update page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My Herbarium",
+  title: "Your Herbarium",
 };
 
 export default function RootLayout({
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -8,7 +8,7 @@ interface HomePageProps {
 export const HomePage: FC<HomePageProps> = ({ flowers }) => {
   return (
     <main className="max-w-prose mx-auto px-4 pb-2 pt-[124px] flex flex-col gap-4">
-      <h1>My Herbarium</h1>
+      <h1>Your Herbarium</h1>
       <Flowers flowers={flowers} />
     </main>
   );
